0324e1b0a0895d3498dc55e56cc19e9c2cb0c1b3
Leonardo <116222412+leopsdev@users.noreply.github.com>
2023-11-28 21:17:00 -0300

    JWS token v1

---
+++ src/main/java/com/example/smartcontrol/infra/security/TokenService.java
@@ -0,0 +1,49 @@
+package com.example.smartcontrol.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.example.smartcontrol.domain.user.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTCreationException exception){
+            return "";
+        }
+    }
+
+    public String generationToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch(JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar Token", exception);
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
